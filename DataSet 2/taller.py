109a910fb88a89b87042e87b601078f0883aa83a
Nelson David <nelso570@gmail.com>
2022-05-18 23:58:49 -0500

    DataSet 2 segundos cambios

--- DataSet 2/taller.py
+++ DataSet 2/taller.py
@@ -15,3 +15,24 @@ rangos = [1,5,10,15]
 nombres = ['1','2','3']
 data.Pregnancies = pd.cut(data.Pregnancies, rangos, labels=nombres)
 data.Pregnancies.replace(np.nan, 1, inplace=True)
+
+rangos2 = [0,10,30,50,100]
+nombres2 = ['1','2','3','4']
+data.Glucose = pd.cut(data.Glucose, rangos2, labels=nombres2)
+
+rangos3 = [0,50,100,150]
+nombres3 = ['1','2','3']
+data.BloodPressure = pd.cut(data.BloodPressure, rangos3, labels=nombres3)
+
+rangos4 = [0,100,200,300,400]
+nombres4 = ['1','2','3','4']
+data.Insulin = pd.cut(data.Insulin, rangos4, labels=nombres4)
+
+rangos5 = [0,20,40,60]
+nombres5 = ['1','2','3']
+data.BMI = pd.cut(data.BMI, rangos5, labels=nombres5)
+
+
+rangos6 = [5,10,15,20]
+nombres6 = ['1','2','3']
+data.Age = pd.cut(data.Age, rangos6, labels=nombres6)
