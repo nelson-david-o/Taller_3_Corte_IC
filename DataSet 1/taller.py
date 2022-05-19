7cfb6f21145ba916c55dcd2f172e546798f12f74
Nelson David <nelso570@gmail.com>
2022-05-18 23:15:14 -0500

    Parte de validacion cruzada

--- DataSet 1/taller.py
+++ DataSet 1/taller.py
@@ -70,3 +70,6 @@ for train, test in kfold.split(x, y):
     acc_scores_test_train.append(scores_test_train)
     
 y_pred = logreg.predict(x_test_out)
+
+print('*'*50)
+print('Regresión Logística Validación cruzada')
