55a497ae1aa087dd7b7bcd87b845fec76470584a
Nelson David <nelso570@gmail.com>
2022-05-18 22:55:15 -0500

    quinto cambio

--- DataSet 1/taller.py
+++ DataSet 1/taller.py
@@ -19,3 +19,16 @@ from sklearn.metrics import f1_score
 
 url = 'bank-full.csv'
 data = pd.read_csv(url)
+
+# tratamiento de la data de bank-full
+
+data.default.replace(['no','yes'], [0,1], inplace= True)
+data.job.replace(['blue-collar','management','technician','admin.','services','retired','self-employed','entrepreneur','unemployed','housemaid','student','unknown'], 
+[0,1,2,3,4,5,6,7,8,9,10,11], inplace= True)
+data.marital.replace(['married','single','divorced'], [0,1,2], inplace= True)
+data.education.replace(['secondary','tertiary','primary','unknown'], [0,1,2,3], inplace= True)
+data.housing.replace(['no','yes'], [0,1], inplace= True)
+data.loan.replace(['no','yes'], [0,1], inplace= True)
+data.contact.replace(['cellular','unknown','telephone'], [0,1,2], inplace= True)
+data.poutcome.replace(['unknown','failure','other','success'], [0,1,2,3], inplace= True)
+data.y.replace(['no','yes'], [0,1], inplace= True)
