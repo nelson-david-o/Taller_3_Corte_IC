12ccf7f307aa997b8378109ca60f7d6f5c3db6a6
Nelson David <nelso570@gmail.com>
2022-05-19 00:01:22 -0500

    DataSet 2 accuracys

--- DataSet 2/taller.py
+++ DataSet 2/taller.py
@@ -79,3 +79,12 @@ print('Regresión Logística Validación cruzada')
 
 print('*'*50)
 print('Regresión Logística')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {logreg.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
