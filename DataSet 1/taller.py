e9fe78fe1d9b009f2fa2e164be24b44e76bb6a0f
Nelson David <nelso570@gmail.com>
2022-05-18 22:49:10 -0500

    tercer cambio

--- DataSet 1/taller.py
+++ DataSet 1/taller.py
@@ -2,3 +2,18 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from warnings import simplefilter
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
